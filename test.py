63d014c8018970abd723b593fff330b59c83ae02
BuildTools <unconfigured@null.spigotmc.org>
2022-12-17 00:43:27 +0800

    Edited test.py
    開始練習OOP程式12/17

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
-print("Hello")
-print("World")
-print("!!!")
+class test:
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+        print(a,b)
+    def sum(self,x,y):
+        return x*y
+
+test_class = test(100,5)
+test_class.a = 200
+print(test_class.sum(test_class.a,test_class.b))
