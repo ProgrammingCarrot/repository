22a87da5e73d14b96a79c7777a16d8b05337cea8
BuildTools <unconfigured@null.spigotmc.org>
2022-12-17 13:01:38 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 class test:
-    def __init__(self,a,b):
+    def __init__(self,a: int,b = 0):
+        assert a >= 0,f"a {a} must greater than or equal to zero"
+        assert b >= 0,f"b {b} must greater than or equal to zero"
         self.a = a
         self.b = b
         print(a,b)
-    def sum(self,x,y):
-        return x*y
+    def sum(self):
+        return self.a * self.b
 
-test_class = test(100,5)
+test_class = test(100,-5)
+test_class_2 = test(1100)
 test_class.a = 200
-print(test_class.sum(test_class.a,test_class.b))
+print(test_class.sum())
+print(test_class_2.sum())
